fd1112ab43717cfc19c51a6d9367924f6b766cd7
Captain Ratax <github.darkness575@passinbox.com>
2023-12-02 21:57:21 +0000

    Added better formats to the wallpaper image when supported by the browser

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ targets.forEach((element) => {
     target.style.display = "flex";
   });
 });
+
+/* Image format detection and background image setting */
+
+window.addEventListener('load', () => {
+  const img = document.querySelector('#background-image-support img');
+  let imageUrl;
+
+  switch (img.currentSrc.split('.').pop()) {
+    case 'avif':
+      imageUrl = '../img/wallpaper.avif';
+      break;
+    case 'webp':
+      imageUrl = '../img/wallpaper.webp';
+      break;
+    default:
+      imageUrl = '../img/wallpaper.png';
+      break;
+  }
+
+  document.documentElement.style.setProperty('--background_image', `url("${imageUrl}")`);
+});
